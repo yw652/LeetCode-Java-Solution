9002ff6c0bce617f88c990fc5c2b25e351ee3b45
Yitong Wang <yw652@nyu.edu>
2015-02-18 10:21:34 -0500

    Update MergeTwoSortedList.java

--- yw652/MergeTwoSortedList.java
+++ yw652/MergeTwoSortedList.java
@@ -1,5 +1,8 @@
 package yw652;
 
+//Merge two sorted linked lists and return it as a new list. 
+//The new list should be made by splicing together the nodes of the first two lists.
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
